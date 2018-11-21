befed8907a5e9a5ebb345c617ea94f641753f95e
Savaş Cilve <savascilve@outlook.com>
2018-11-21 16:05:43 +0000

    Implemented Save and All

---
+++ Smartspace.Repository.Test/InMemoryRepositoryShould.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Smartspace.Repository.Interfaces;
+
+namespace Smartspace.Repository.Test
+{
+    [TestFixture]
+    public class InMemoryRepositoryShould
+    {
+        private IRepository<IStoreable> _repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repository = new InMemoryRepository<IStoreable>();
+        }
+
+        [Test]
+        public void AddEntityToMemory_WhenGivenEntityIsIStoreable()
+        {
+            var item = new Mock<IStoreable>();
+            var expectedCount = 1;
+
+            _repository.Save(item.Object);
+
+            Assert.AreEqual(expectedCount, _repository.All().Count());
+        }
+    }
+}
---
+++ Smartspace.Repository/InMemoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Smartspace.Repository.Interfaces;
+
+namespace Smartspace.Repository
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : IStoreable
+    {
+        private readonly List<T> _entityList;
+
+        public InMemoryRepository()
+        {
+            _entityList = new List<T>();
+        }
+
+        public IEnumerable<T> All()
+        {
+            return _entityList;
+        }
+
+        public void Delete(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(T item)
+        {
+            _entityList.Add(item);
+        }
+    }
+}
